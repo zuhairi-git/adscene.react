a0add758e0a857afeeeea7155728d9b94e5c8b06
Ali Al-Zuhairi <zohairi@live.com>
2022-12-16 22:13:02 +0200

    Add className

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -4,20 +4,23 @@ import 'bootstrap/dist/css/bootstrap.css';
 const Header = () => {
   return (
     <>
-      <header id="header" class="fixed-top d-flex align-items-center">
-        <div class="container d-flex align-items-center">
+      <header id="header" className="fixed-top d-flex align-items-center">
+        <div className="container d-flex align-items-center">
 
-          <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid" /></a>
+          <a href="index.html" className="logo me-auto"><img src="assets/img/logo.png" alt="" className="img-fluid" /></a>
 
-          <nav id="navbar" class="navbar">
+          <nav id="navbar" className="navbar">
             <ul>
               <li><Link to="/">Home</Link></li>
               <li><Link to="/portfolio">Portfolio</Link></li>
               <li><Link to="/services">Services</Link></li>
               <li><Link to="/about">About</Link></li>
-              <li className="d-none"><Link to="/contact" class="getstarted">Get Started</Link></li>
+              <li className="d-none"><Link to="/contact" className="getstarted">Get Started</Link></li>
             </ul>
-            <i class="bi mobile-nav-toggle bi-list"></i>
+            <button className="d-none navbar-toggler" type="button" data-toggle="collapse" data-target="/navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <i className="bi mobile-nav-toggle bi-list"></i>
           </nav>
 
         </div>
